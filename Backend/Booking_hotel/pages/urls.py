1f6abc67ab5aa7182de366fb4080d9363e72c857
maiyahia <80985649+maiyahia@users.noreply.github.com>
2025-05-02 17:40:49 +0300

    Update urls.py

--- Backend/Booking_hotel/pages/urls.py
+++ Backend/Booking_hotel/pages/urls.py
@@ -1,13 +1,3 @@
-# from django.urls import path
-# from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-# from .views import RegisterView
-
-# urlpatterns = [
-#     path('register/', RegisterView.as_view(), name='register'),
-#     path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-#     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-# ]
-
 from django.urls import path, include
 from .views import RegisterView
 from django.contrib import admin
