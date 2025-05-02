21b75bed6548035dc283c9d64d34c691a3f66bf3
maiyahia <80985649+maiyahia@users.noreply.github.com>
2025-05-02 17:29:54 +0300

    Update views.py

--- Backend/Booking_hotel/pages/views.py
+++ Backend/Booking_hotel/pages/views.py
@@ -1,32 +1,3 @@
-# from django.shortcuts import render
-
-# from rest_framework import generics
-# from django.contrib.auth.models import User
-# from rest_framework.permissions import AllowAny
-# from rest_framework.serializers import ModelSerializer
-
-# # Create Serializer
-# class RegisterSerializer(ModelSerializer):
-#     class Meta:
-#         model = User
-#         fields = ('id', 'username', 'email', 'password')
-#         extra_kwargs = {'password': {'write_only': True}}
-
-#     def create(self, validated_data):
-#         user = User.objects.create_user(
-#             username=validated_data['username'],
-#             email=validated_data['email'],
-#             password=validated_data['password']
-#         )
-#         return user
-
-# # Create View
-# class RegisterView(generics.CreateAPIView):
-#     queryset = User.objects.all()
-#     permission_classes = (AllowAny,)
-#     serializer_class = RegisterSerializer
-
-
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
