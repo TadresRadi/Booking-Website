d9f1e9dfacfc485a45844574edf4be0612000aa6
Esraa_salman <salman77esraa@gmail.com>
2025-05-03 02:16:25 +0200

    login and registeration without like backend

--- frontend/Front_app/src/App.jsx
+++ frontend/Front_app/src/App.jsx
@@ -2,12 +2,15 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import { Register } from './page/register/register';
 import { Add_property } from './page/hostone/host';
+import { Login } from './page/login/login';
 
 function App() {
   return (
     <Router>
       <Routes>
+
         <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/booking" element={<Add_property />} />
         <Route path="*" element={<Register />} />
       </Routes>
