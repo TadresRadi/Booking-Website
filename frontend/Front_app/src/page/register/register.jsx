8ebb9c20e7ca4b6bcc9bf090ce7324519140d432
Esraa_salman <salman77esraa@gmail.com>
2025-05-09 08:32:11 +0200

    .

--- frontend/Front_app/src/page/register/register.jsx
+++ frontend/Front_app/src/page/register/register.jsx
@@ -23,7 +23,7 @@ export function Register() {
       accept_terms: data.accept_terms,
     };
 
-    const response = await fetch('http://127.0.0.1:8000/api/register/', {
+    const response = await fetch('http://127.0.0.1:5000/api/register/', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
