d2f2207264c0972414c46760fde50507eaa7a7bc
maiyahia <80985649+maiyahia@users.noreply.github.com>
2025-05-10 03:43:45 +0300

    Update serializers.py

--- Backend/Booking_hotel/pages/serializers.py
+++ Backend/Booking_hotel/pages/serializers.py
@@ -1,5 +1,9 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
+from .models import Hotel
+from .models import Room
+from .models import Details
+from .models import HotelPhoto
 
 class RegisterSerializer(serializers.ModelSerializer):
     first_name = serializers.CharField()
@@ -26,12 +30,15 @@ class RegisterSerializer(serializers.ModelSerializer):
         validated_data.pop('accept_terms')
 
         
-        base_username = validated_data['email'].split('@')[0].lower()
+        first_name = validated_data['first_name'].strip().lower()
+        last_name = validated_data['last_name'].strip().lower()
+        base_username = f"{first_name}_{last_name}"
         username = base_username
         counter = 1
+
         while User.objects.filter(username=username).exists():
-            username = f"{base_username}{counter}"
-            counter += 1
+           username = f"{base_username}{counter}"
+           counter += 1
 
         user = User.objects.create_user(
             username=username,
@@ -41,4 +48,27 @@ class RegisterSerializer(serializers.ModelSerializer):
             password=validated_data['password']
         )
         return user
+    
+class HotelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Hotel
+        fields = '__all__'
+
+
+class RoomSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Room
+        fields = '__all__'
+
+class DetailsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Details
+        fields = '__all__'
+
+
+
+class HotelPhotoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = HotelPhoto
+        fields = ['id', 'hotel', 'image', 'uploaded_at']
 
