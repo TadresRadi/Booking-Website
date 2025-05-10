12f900339315252c2319f79acb9d42625658abff
maiyahia <80985649+maiyahia@users.noreply.github.com>
2025-05-10 03:53:33 +0300

    Update urls.py

--- Backend/Booking_hotel/pages/urls.py
+++ Backend/Booking_hotel/pages/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
-from .views import RegisterView
+from .views import RegisterView, AddHotelView, AddRoomView,   AddDetails, UploadHotelPhotosView
 from django.contrib import admin
+
+
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
@@ -9,5 +11,10 @@ urlpatterns = [
     path('auth/', include('dj_rest_auth.urls')),  
     path('auth/registration/', include('dj_rest_auth.registration.urls')),  
     path('auth/social/', include('allauth.socialaccount.urls')),
-    path('admin/', admin.site.urls)
+    path('admin/', admin.site.urls),
+
+    path('add-hotel/', AddHotelView.as_view(), name='add-hotel'),
+    path('add-room/', AddRoomView.as_view(), name='add-room'),
+    path('add-details/', AddDetails.as_view(), name='add-details'),
+    path('upload-photo/', UploadHotelPhotosView.as_view(), name='upload-hotel-photo'),
 ]
