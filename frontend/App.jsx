afa6048d9fc0a0ed2168d5b2ffdf0013a78ac849
maiyahia <80985649+maiyahia@users.noreply.github.com>
2025-05-12 22:29:11 +0300

    Update App.jsx

--- frontend/App.jsx
+++ frontend/App.jsx
@@ -1,5 +1,4 @@
 import './App.css'
-// import   { Add_property } from './components/Add_hotel/add_hotel.jsx';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import  AddHotelForm  from './components/Add_hotel/add_hotel.jsx';
 
