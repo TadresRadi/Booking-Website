76c3b2b5d8671be9aa554f0a5ccf884ecd144fc1
maiyahia <80985649+maiyahia@users.noreply.github.com>
2025-05-16 17:37:52 +0300

    Update urls.py

--- Backend/Booking_hotel/pages/urls.py
+++ Backend/Booking_hotel/pages/urls.py
@@ -4,6 +4,8 @@ from django.contrib import admin
 from .views import LoginView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from . import views
+from .views import RegisterView, AddHotelView, AddRoomView,   AddDetailsView, UploadHotelPhotosView, UploadRoomPhotosView
+
 
 
 urlpatterns = [
@@ -18,4 +20,10 @@ urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/register/', views.register_user),
+     path('add-hotel/', AddHotelView.as_view(), name='add-hotel'),
+    path('add-room/', AddRoomView.as_view(), name='add-room'),
+    path('add-details/', AddDetailsView.as_view(),     name='add-details'),
+    path('AddHotelImages/', UploadHotelPhotosView.as_view(), name='upload-hotel-photo'),
+    path('AddRoomImages/', UploadRoomPhotosView.as_view(), name='upload-room-photo'),
 ]
+ 
