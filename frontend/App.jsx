0960d79112fdeab185816c1618bef0481ddd9087
maiyahia <80985649+maiyahia@users.noreply.github.com>
2025-05-17 04:14:03 +0300

    Update App.jsx

--- frontend/App.jsx
+++ frontend/App.jsx
@@ -4,6 +4,7 @@ import  AddHotelForm  from './components/Add_hotel/add_hotel.jsx';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import AddRoomForm from './components/Add_room/add_room.jsx';
 import FinalStepForm from './components/Add_details/add_details.jsx';
+import AddPhotosPage from './components/Add_Images/add_images.jsx';
 
 
 
@@ -14,6 +15,7 @@ function App() {
     <>
     <BrowserRouter>
     <Routes>
+      <Route path='Add-Images' element={<AddPhotosPage />} />
       <Route path='Add-Hotel' element={<AddHotelForm/>}/>
       <Route path='Add-Room' element={<AddRoomForm/>}/>
       <Route path='Add-Details' element={<FinalStepForm/>}/>
