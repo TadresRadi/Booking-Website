0f621dda40d723701a1119dbe8ad2af03f561d2b
maiyahia <80985649+maiyahia@users.noreply.github.com>
2025-05-20 18:46:44 +0300

    Update models.py

--- Backend/Booking_hotel/pages/models.py
+++ Backend/Booking_hotel/pages/models.py
@@ -133,21 +133,3 @@ class Review(models.Model):
         return f"Image for {self.room.name}"
 
 
-class Details(models.Model):
-    hotel = models.OneToOneField(Hotel, on_delete=models.CASCADE, related_name='details')
-
-    visa = models.BooleanField(default=False)
-    mastercard = models.BooleanField(default=False)
-    meeza_card = models.BooleanField(default=False)
-    cash_on_delivery = models.BooleanField(default=False)
-    etsalat_cash = models.BooleanField(default=False)
-    aman_payment = models.BooleanField(default=False)
-    orange_cash = models.BooleanField(default=False)
-    vodafone_cash = models.BooleanField(default=False)
-    fawny_pay = models.BooleanField(default=False)
-
-    allow_children = models.BooleanField(default=False)
-    allow_pets = models.BooleanField(default=False)
-
-    def _str_(self):
-        return f"Details for {self.hotel.name}"
