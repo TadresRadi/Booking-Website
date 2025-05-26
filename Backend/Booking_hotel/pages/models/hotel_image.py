a6329d132293350aaae0eebf6558d1eaee286d56
maiyahia <80985649+maiyahia@users.noreply.github.com>
2025-05-26 13:27:15 +0300

    Update hotel_image.py

--- Backend/Booking_hotel/pages/models/hotel_image.py
+++ Backend/Booking_hotel/pages/models/hotel_image.py
@@ -5,5 +5,6 @@ from .hotel import Hotel
 class HotelPhoto(models.Model):
     hotel = models.ForeignKey(Hotel, related_name="hotel_images",on_delete=models.CASCADE)
     image = models.ImageField( upload_to='hotel_images/' ,height_field=None, width_field=None, max_length=None, null=True, blank=True)
+    is_main = models.BooleanField(default=False) 
     def __str__(self):
         return f"Photo for {self.hotel.hotel_name}"
